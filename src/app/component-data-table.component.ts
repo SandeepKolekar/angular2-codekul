8da54f6d1a864d9e8f9c4bb07686d047c6689cda
SandeepKolekar <sandeepkolekar4@gmail.com>
2017-10-15 22:20:03 +0530

    Modified data table style and sort and filter design

--- src/app/component-data-table.component.ts
+++ src/app/component-data-table.component.ts
@@ -9,21 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class ComponentDataTableComponent implements OnInit {
 
   employeeData = [];
+  idSortFlag = false ;
+  nameSortFlag = false ;
+  emailSortFlag = false ;
+  mobileSortFlag = false ;
   constructor() { }
+  tableSort(sortValue , sortFlag) {
+    alert(sortFlag + ' sort under constraction!');
+    return  sortValue ;
+  }
+
+  tableFilter(filterValue , filterFlag) {
+    alert('filter under constraction!');
+  }
 
   ngOnInit() {
     this.employeeData = [
       {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 2, name: 'manisha kadam', email: 'manisha@gmail.com', mobile: 4525255555},
-      {id : 2, name: 'manisha kadam', email: 'manisha@gmail.com', mobile: 4525255555},
-      {id : 2, name: 'manisha kadam', email: 'manisha@gmail.com', mobile: 4525255555},
       {id : 2, name: 'manisha kadam', email: 'manisha@gmail.com', mobile: 4525255555},
       {id : 3, name: 'kedar medhekar', email: 'kedar@gmail.com', mobile: 4525125255
       }
