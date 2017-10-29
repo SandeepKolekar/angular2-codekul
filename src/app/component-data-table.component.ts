4b2bf6b63d0d93d7c4c7362ae2744f5d813c35df
SandeepKolekar <sandeepkolekar4@gmail.com>
2017-10-29 21:05:44 +0530

    NEW UPDATEW

--- src/app/component-data-table.component.ts
+++ src/app/component-data-table.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-component-data-table',
   templateUrl: './data.table.component.html',
@@ -9,26 +8,73 @@ import { Component, OnInit } from '@angular/core';
 export class ComponentDataTableComponent implements OnInit {
 
   employeeData = [];
+  employeeTempData = [];
   idSortFlag = false ;
   nameSortFlag = false ;
   emailSortFlag = false ;
   mobileSortFlag = false ;
   constructor() { }
+  // Table coulmn sort
   tableSort(sortValue , sortFlag) {
-    alert(sortFlag + ' sort under constraction!');
+
+    this.employeeData.sort((a, b) => {
+      let aPz = a[sortFlag];
+      let bPz = b[sortFlag];
+
+      if (sortValue) {
+        return bPz - aPz;
+       } else {
+        return aPz - bPz;
+       }
+    });
+/*
+    this.employeeData.sort( function(employeeDataA, employeeDataB) {
+	    if ( employeeDataA[sortFlag] < employeeDataB[sortFlag] ){
+	    	return -1;
+	    }else if( employeeDataA[sortFlag] > employeeDataB[] ){
+	        return 1;
+	    }else{
+	    	return 0;
+	    }
+	});
+
+*/
     return  sortValue ;
   }
 
-  tableFilter(filterValue , filterFlag) {
-    alert('filter under constraction!');
+  // Table filter
+  tableFilter( id , name , email , mobile) {
+    this.employeeData = this.employeeTempData ;
+
+    if (id) {
+      this.employeeData = this.employeeData.filter( data =>  (data.id).includes(id));
+    }
+    if (name) {
+      this.employeeData = this.employeeData.filter( data =>  (data.name).includes(name));
+    }
+    if (email) {
+      this.employeeData = this.employeeData.filter( data =>  (data.email).includes(email)); 
+    }
+    if (mobile) {
+      this.employeeData = this.employeeData.filter( data =>  (data.mobile).includes(mobile));
+    }
   }
 
   ngOnInit() {
     this.employeeData = [
-      {id : 1, name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: 9857585525},
-      {id : 2, name: 'manisha kadam', email: 'manisha@gmail.com', mobile: 4525255555},
-      {id : 3, name: 'kedar medhekar', email: 'kedar@gmail.com', mobile: 4525125255
+      {id : '1', name: 'sandeep kolekar', email: 'sandeep@gmail.com', mobile: '9857585525'},
+      {id : '3', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '2', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '2', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '4', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '1455555555'},
+      {id : '2', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '2', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '1455555555'},
+      {id : '2', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '11', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '2', name: 'manisha kadam', email: 'manisha@gmail.com', mobile: '4525255555'},
+      {id : '3', name: 'kedar medhekar', email: 'kedar@gmail.com', mobile: '4525125255'
       }
     ];
+    this.employeeTempData = this.employeeData  ;
   }
 }
